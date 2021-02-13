f399c99dd615a82683396060a9205393e2d526a6
KuicsCristian <kuics.cristian@gmail.com>
2021-02-13 17:29:01 +0200

    Add project files.

---
+++ leave-management/Mappings/Maps.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using leave_management.Data;
+using leave_management.Models;
+
+namespace leave_management.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+        }
+    }
+}
